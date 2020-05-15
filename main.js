2618f3d3d5189a07b477028864cfd2ba0539125b
AnandaCba <fabiogurudeva@gmail.com>
2020-05-15 17:59:55 -0300

    Refatorando codigo

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 //Criada a função para realizar o calculo
 function transform(){
-    //Variável buscando o valor inserido pelo User
-    var b1 = Number(document.getElementById('bin1').value)
+
     //Caso o user tenha inserido valor "1" a condição joga o valor do número que representa na variável "br1"
     //Cada posição tem um valor especifico no qual vai receber esse valor se a informação que o user passou por "1"
     //Então isso se repete por 8 vezes, as 8 posições disponíveis para o user inserir valores
+    
+    //Variável buscando o valor inserido pelo User
+    var b1 = Number(document.getElementById('bin1').value)
+    console.log(b1)
     var br1 = 0;
         if (b1==1){
             br1 = 128;
             console.log(br1)
+            
         }  //Condição para verificar se o number é ou não binário. Se não for surge um alerta
     //Inicia todo o processo novamente
     var b2 = Number(document.getElementById('bin2').value)
+    console.log(b2)
     var br2 = 0;
         if (b2==1){
             var br2 = 64;
@@ -54,12 +59,14 @@ function transform(){
         console.log(br8)
     }
     // Verificação se algum input é >1 || < 0 
-    if (b1 || b2 || b3 || b4 || b5 || b6 || b7 || b8 != 1 && 0 )
-    {
+    if (b1 > 1 || b2 > 1 || b3 > 1 || b4 >1 || b5 >1 || b6>1 || b7>1 || b8>1 ){
         window.alert('Digite apenas números binários (0 ou 1)')
     }
+    if (b1 < 0 || b2 < 0 || b3 <0 || b4 <0 || b5 <0 || b6<0 || b7<0 || b8<0){
+        window.alert('Digite apenas números binários (0 ou 1)')
+    } else {
     //Soma-se todos os valores e exibe na tela
     var soma = br1 + br2 + br3 + br4 + br5 + br6 + br7 + br8;
     res.innerHTML =  `${soma}`
-
+    }
 }
