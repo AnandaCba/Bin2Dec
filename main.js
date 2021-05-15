0821e829e0ee0f14e2a7dbb7d9c6a7500d1b1375
anandacba <fabiogurudeva@gmail.com>
2021-05-15 02:05:42 -0300

    Code refactoring and changing the algorithm

--- main.js
+++ main.js
@@ -1,84 +1,27 @@
-//Criada a função para realizar o calculo
-function transform(){
+function bintodec(){
 
-    //Caso o user tenha inserido valor "1" a condição joga o valor do número que representa na variável "br1"
-    //Cada posição tem um valor especifico no qual vai receber esse valor se a informação que o user passou por "1"
-    //Então isso se repete por 8 vezes, as 8 posições disponíveis para o user inserir valores
+    let total = 0;
+    let exponentiation = 0;
 
-    // Vetor para armazenar number do user
-    var arr = []
+    //Buscando valores inseridos
+    binNumber = document.getElementById('bin1').value;
+    binaryLength = binNumber.length - 1;
     
-    var br1=0, 
-        br2=0, 
-        br3=0, 
-        br4=0, 
-        br5=0, 
-        br6=0, 
-        br7=0, 
-        br8=0
-    //Variável buscando o valor inserido pelo User
-    var b1 = Number(document.getElementById('bin1').value)
-    // Push para o array
-    arr.push(b1)
-        //Verificação...
-        if (b1==1){
-            br1 = 128;
-            console.log(br1)
-        }
-    //Inicia todo o processo novamente
-    var b2 = Number(document.getElementById('bin2').value)
-    console.log(b2)
-    arr.push(b2)
-        if (b2==1){
-            var br2 = 64;
-            console.log(br2)
-        }
-    var b3 = Number(document.getElementById('bin3').value)
-    arr.push(b3)
-        if (b3==1){
-            var br3 = 32;
-            console.log(br3)
-        }
-    var b4 = Number(document.getElementById('bin4').value)
-    arr.push(b4)
-        if (b4==1){
-            var br4 = 16;
-            console.log(br4)
-        }
-    var b5 = Number(document.getElementById('bin5').value)
-    arr.push(b5)
-    if (b5==1){
-        var br5 = 8;
-        console.log(br5)
-    }
-    var b6 = Number(document.getElementById('bin6').value)
-    arr.push(b6)
-    if (b6==1){
-        var br6 = 4;
-        console.log(br6)
-    }
-    var b7 = Number(document.getElementById('bin7').value)
-    arr.push(b7)
-    if (b7==1){
-        var br7 = 2;
-        console.log(br7)
-    }
-    var b8 = Number(document.getElementById('bin8').value)
-    arr.push(b8)
-    if (b8==1){
-        var br8 = 1;
-        console.log(br8)
-    }
-    // Verificação se algum input é >1 || < 0 
-    for(c = 0; c <= 9 ; c++){
-        if (arr[c] > 1 ){
-            window.alert('Digite apenas números binários (0 ou 1)')
-            }
-        if (arr[c] < 0){
-            window.alert('Digite apenas números binários (0 ou 1)')
-        }        
-    }
-    //Soma-se todos os valores e exibe na tela
-    var soma = br1 + br2 + br3 + br4 + br5 + br6 + br7 + br8;
-    res.innerHTML =  `${soma}`
-}
+    //Cálculo para conversão de binário para decimal
+    for(binaryLength; binaryLength >= 0; binaryLength-- ){
+
+        numberValuePosition = Number(binNumber.substr(binaryLength,1))
+        decimalValue = numberValuePosition * Math.pow(2,exponentiation)
+        total = total + decimalValue
+
+        exponentiation = exponentiation + 1
+    };
+    //Exibe resultado
+    result.innerHTML =  `${total}`
+};
+
+//Foco ao abrir a pagina e limpa a área
+window.onload = function() {
+    document.getElementById("bin1").focus();
+    document.querySelector('textarea').value = '';
+  };
